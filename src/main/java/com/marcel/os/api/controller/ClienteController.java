fd8d58067102c3a166ef1c64ec97db906c5caac1
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2020-04-23 11:35:17 -0300

    Implementação das Validações para o domínio Cliente. Até aqui só Cliente

--- src/main/java/com/marcel/os/api/controller/ClienteController.java
+++ src/main/java/com/marcel/os/api/controller/ClienteController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -36,13 +37,13 @@ public class ClienteController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Cliente adicionar(@RequestBody Cliente cliente){
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente){
         return clienteRepository.save(cliente);
     }
 
     @PutMapping("/{clienteId}")
     @ResponseStatus()
-    public ResponseEntity<Cliente> atualizar(@PathVariable Long clienteId,
+    public ResponseEntity<Cliente> atualizar(@Valid @PathVariable Long clienteId,
                                              @RequestBody Cliente cliente){
         if (!clienteRepository.existsById(clienteId)){
             return ResponseEntity.notFound().build();
--- src/main/java/com/marcel/os/domain/model/Cliente.java
+++ src/main/java/com/marcel/os/domain/model/Cliente.java
@@ -1,6 +1,9 @@
 package com.marcel.os.domain.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Cliente {
@@ -8,8 +11,18 @@ public class Cliente {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @NotBlank
+    @Size(max = 60)
     private String nome;
+
+    @NotBlank
+    @Email
+    @Size(max = 255)
     private String email;
+
+    @NotBlank
+    @Size(max = 20)
     private String telefone;
 
     public Long getId() {
