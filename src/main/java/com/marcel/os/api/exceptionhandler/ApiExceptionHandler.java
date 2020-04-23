5127139f65a89a304e6cf738368f888d3f10f65a
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2020-04-23 14:33:32 -0300

    Implementação das exceções com arquivo para internacionalização das exceções no domínio Cliente

--- src/main/java/com/marcel/os/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/marcel/os/api/exceptionhandler/ApiExceptionHandler.java
@@ -1,5 +1,8 @@
 package com.marcel.os.api.exceptionhandler;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +11,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
@@ -16,6 +20,9 @@ import java.util.ArrayList;
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
+    @Autowired
+    private MessageSource messageSource;
+
     @Override
     protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
                                                                   HttpHeaders headers, HttpStatus status, WebRequest request) {
@@ -23,7 +30,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
 
         for (ObjectError error : ex.getBindingResult().getAllErrors()){
             String nome = ((FieldError) error).getField();
-            String mensagem = error.getDefaultMessage();
+            String mensagem = messageSource.getMessage(error, LocaleContextHolder.getLocale());
 
             campos.add(new Problema.Campo(nome, mensagem));
         }
