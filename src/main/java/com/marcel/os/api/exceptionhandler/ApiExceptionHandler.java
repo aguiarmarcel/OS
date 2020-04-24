8bddec3f72cc70c033107e03ccce7600a9b65894
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2020-04-24 02:11:32 -0300

    mudança do formato da data de LocalDateTime para OffsetDateTime

--- src/main/java/com/marcel/os/api/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/com/marcel/os/api/exceptionhandler/ApiExceptionHandler.java
@@ -17,6 +17,7 @@ import org.springframework.web.servlet.LocaleContextResolver;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.util.ArrayList;
 
 @ControllerAdvice
@@ -31,7 +32,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         var problema = new Problema();
         problema.setStatus(status.value());
         problema.setTitulo(ex.getMessage());
-        problema.setDataHora(LocalDateTime.now());
+        problema.setDataHora(OffsetDateTime.now());
 
         return handleExceptionInternal(ex, problema, new HttpHeaders(), status, resquest);
     }
@@ -51,7 +52,7 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         var problema = new Problema();
         problema.setStatus(status.value());
         problema.setTitulo("Um ou mais campos estão inválidos. Faça o prenchimento correto e tente novamente.");
-        problema.setDataHora(LocalDateTime.now());
+        problema.setDataHora(OffsetDateTime.now());
         problema.setCampos(campos);
 
 
--- src/main/java/com/marcel/os/domain/model/OrdemServico.java
+++ src/main/java/com/marcel/os/domain/model/OrdemServico.java
@@ -13,6 +13,7 @@ import javax.validation.groups.ConvertGroup;
 import javax.validation.groups.Default;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 
 @Entity
 public class OrdemServico {
@@ -38,10 +39,10 @@ public class OrdemServico {
     private StatusOrdemServico status;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private LocalDateTime dataAbertura;
+    private OffsetDateTime dataAbertura;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    private LocalDateTime dataFinalizacao;
+    private OffsetDateTime dataFinalizacao;
 
     public Long getId() {
         return id;
@@ -83,19 +84,19 @@ public class OrdemServico {
         this.status = status;
     }
 
-    public LocalDateTime getDataAbertura() {
+    public OffsetDateTime getDataAbertura() {
         return dataAbertura;
     }
 
-    public void setDataAbertura(LocalDateTime dataAbertura) {
+    public void setDataAbertura(OffsetDateTime dataAbertura) {
         this.dataAbertura = dataAbertura;
     }
 
-    public LocalDateTime getDataFinalizacao() {
+    public OffsetDateTime getDataFinalizacao() {
         return dataFinalizacao;
     }
 
-    public void setDataFinalizacao(LocalDateTime dataFinalizacao) {
+    public void setDataFinalizacao(OffsetDateTime dataFinalizacao) {
         this.dataFinalizacao = dataFinalizacao;
     }
 
