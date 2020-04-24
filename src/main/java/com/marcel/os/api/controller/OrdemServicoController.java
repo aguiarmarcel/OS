93ec80f59acb554cf2f2c0a5983636959f656cda
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2020-04-24 01:56:06 -0300

    Implementação do listar e buscar das ordens de serviços.

--- src/main/java/com/marcel/os/api/controller/OrdemServicoController.java
+++ src/main/java/com/marcel/os/api/controller/OrdemServicoController.java
@@ -6,10 +6,12 @@ import com.marcel.os.domain.repository.OrdemServicoRepository;
 import com.marcel.os.domain.service.GestaoOrdemServicoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/ordens-servico")
@@ -18,10 +20,28 @@ public class OrdemServicoController {
     @Autowired
     private GestaoOrdemServicoService gestaoOrdemServicoService;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public OrdemServico criar(@Valid @RequestBody OrdemServico ordemServico){
 
         return gestaoOrdemServicoService.criar(ordemServico);
+    }
+
+    @GetMapping
+    public List<OrdemServico> listar(){
+        return ordemServicoRepository.findAll();
+    }
+
+    @GetMapping("/{ordemServicoId}")
+    public ResponseEntity<OrdemServico> buscar(@PathVariable Long ordemServicoId){
+        Optional<OrdemServico> ordemServico = ordemServicoRepository.findById(ordemServicoId);
+        if (ordemServico.isPresent()){
+            return ResponseEntity.ok(ordemServico.get());
+        }
+
+        return ResponseEntity.notFound().build();
     }
 }
