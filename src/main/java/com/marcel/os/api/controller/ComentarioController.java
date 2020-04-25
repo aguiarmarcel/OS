b54071c53ab5e44006272e47d6f7a0ca814dd07d
Marcel Aguiar <aguiar.marcel.bezerra@gmail.com>
2020-04-25 17:53:49 -0300

    Criação do método listar comentários.

--- src/main/java/com/marcel/os/api/controller/ComentarioController.java
+++ src/main/java/com/marcel/os/api/controller/ComentarioController.java
@@ -4,8 +4,10 @@ import com.marcel.os.api.model.ComentarioInput;
 import com.marcel.os.api.model.ComentarioModel;
 import com.marcel.os.api.model.OrdemServicoInput;
 import com.marcel.os.api.model.OrdemServicoModel;
+import com.marcel.os.domain.exception.EntidadeNaoEncontradaException;
 import com.marcel.os.domain.model.Comentario;
 import com.marcel.os.domain.model.OrdemServico;
+import com.marcel.os.domain.repository.OrdemServicoRepository;
 import com.marcel.os.domain.service.GestaoOrdemServicoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,6 +15,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/ordens-servico/{ordemServicoId}/comentarios")
@@ -24,6 +28,9 @@ public class ComentarioController {
     @Autowired
     private GestaoOrdemServicoService gestaoOrdemServicoService;
 
+    @Autowired
+    private OrdemServicoRepository ordemServicoRepository;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public ComentarioModel adicionar(@PathVariable Long ordemServicoId,
@@ -33,8 +40,21 @@ public class ComentarioController {
         return toModel(comentario);
     }
 
+    @GetMapping
+    public List<ComentarioModel> listar(@PathVariable Long ordemServicoId){
+        OrdemServico ordemServico = ordemServicoRepository.findById(ordemServicoId)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Ordem de SErviço não encontrada."));
+        return toCollectionModel(ordemServico.getComentarios());
+    }
+
     //Método para a conversão de Comentario para ComentarioModel
     private ComentarioModel toModel(Comentario comentario){
         return modelMapper.map(comentario, ComentarioModel.class);
+    }
+
+    //Método para passar uma lista de comentarios da ordemServico para uma lista de comentarioModel
+    private List<ComentarioModel> toCollectionModel(List<Comentario> comentarios){
+        return comentarios.stream().map(comentario -> toModel(comentario))
+                .collect(Collectors.toList());
     }
 }
